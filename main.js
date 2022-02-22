16cbf52c027207639080749944382da9cfb76a5f
Dana <danamudrova@gmail.com>
2022-02-22 20:37:34 +0100

    poznamky z hodiny

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 // tady je místo pro náš program
-//testovací commit
+
+
+function hazej(){
+    let kostka = document. querySelector('#kostka') //element,co se bude měnit
+    let zprava = document. querySelector('#zprava') // element, co se bude měnit
+    let cislo = Math.floor(Math.random() * 6 + 1)
+   
+    if (cislo === 6){
+        zprava.textContent = "Vyhrál jsi!";
+    }   else {
+        zprava.textContent = "Je to " + cislo + " Házej znovu";
+    }
+
+    if (cislo > 0 && cislo < 7){
+        kostka.src = 'obrazky/' + cislo + ".png"
+    }   else {
+        console.log("Chyba!")
+    }
+}
